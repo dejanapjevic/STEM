bb1fd8223665dcc1f18548c046efffc0283387f6
Dejana <dejanapjevic@hotmail.com>
2024-11-25 18:30:07 +0100

    Dodata baza stem.db

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import { Outlet, useLocation, useNavigate } from "react-router-dom";
 function App() {
   const location = useLocation();
     const navigate = useNavigate(); 
+
     useEffect(() => {
       if (location.pathname === '/') {
           navigate('/home');
